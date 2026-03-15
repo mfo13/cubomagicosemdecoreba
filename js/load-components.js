85837058b0b9702ff5df54cf2fc78196f9aba6f6
mfo13 <marcelo.falcao.oliveira@gmail.com>
2026-03-15 00:42:46 -0300

    new files

---
+++ js/load-components.js
@@ -0,0 +1,9 @@
+async function loadComponent(id, file)
+{
+const response = await fetch(file);
+const text = await response.text();
+document.getElementById(id).innerHTML = text;
+}
+
+loadComponent("menu","/components/menu.html");
+loadComponent("footer","/components/footer.html");
